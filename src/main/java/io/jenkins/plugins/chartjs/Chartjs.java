8d115103b8c264849dc5966abaa9384bf31df6bd
Jiri <jvanek@redhat.com>
2023-03-16 17:49:19 +0100

    Refactored to io.jenkins.plugins.chartjs

--- src/main/java/io/jenkins/plugins/chartjs/Chartjs.java
+++ src/main/java/io/jenkins/plugins/chartjs/Chartjs.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package hudson.plugins;
+package io.jenkins.plugins.chartjs;
 
 import hudson.Extension;
 import hudson.model.PageDecorator;
--- src/main/java/io/jenkins/plugins/chartjs/ColorChanger.java
+++ src/main/java/io/jenkins/plugins/chartjs/ColorChanger.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package hudson.plugins;
+package io.jenkins.plugins.chartjs;
 
 import java.awt.Color;
 import java.util.Random;
